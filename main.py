fb472f49e22bb561dd0402325a0fd41ddb818229
Alyse Jessup <ajessup@uwaterloo.ca>
2023-11-21 16:10:10 -0500

    Added main and settings

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 16:01:27 2023
+
+@author: alysejessup
+"""
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 16:04:53 2023
+
+@author: alysejessup
+"""
+
